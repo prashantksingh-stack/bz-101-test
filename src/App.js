c792029a0a8973020d222a6b2c1c8675cf33b4b2
Prashant <prashantksingh@beingzero.in>
2025-01-22 20:45:55 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi from Being Zero
+          Hi from Being Zero! Welcome
         </p>
+        <p>Wrote by X pfhgrhefjeerson</p>
         <a
           className="App-link"
           href="https://reactjs.org"
